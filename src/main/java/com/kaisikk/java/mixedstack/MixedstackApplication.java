fbd278730a46bb1d2ac43d568ac9ab6ca8d20906
Kaisikk <Kosikov.Ilya@mail.ru>
2024-04-14 23:20:20 +0400

    Initial commit

---
+++ src/main/java/com/kaisikk/java/mixedstack/MixedstackApplication.java
@@ -0,0 +1,13 @@
+package com.kaisikk.java.mixedstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixedstackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MixedstackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kaisikk/java/mixedstack/MixedstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaisikk.java.mixedstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixedstackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
